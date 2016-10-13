b4c2b79cdc4f1c89f134ff2facbb7400104230a1
sunilkumarcheni <chenisunilkumar1234@gmail.com>
2016-10-13 16:54:41 +0530

    quiz questions
    quiz questions

---
+++ quiz.js
@@ -0,0 +1,4 @@
+var cat1 = ($("input[@name=q1]:checked").val() != "a");
+var cat2 = ($("input[@name=q2]:checked").val() != "b");
+<script type="text/javascript" src="/js/jquery.js"></script>
+<script type="text/javascript" src="/js/quiz-1.js"></script>
